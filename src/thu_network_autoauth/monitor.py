ad949af9f0fe33e634fd49b3e1d156d9ac367b75
2745518585 <zz2745518585@gmail.com>
2026-04-05 00:42:03 +0800

    fix: Incorrect process when testing ip by ping

--- src/thu_network_autoauth/monitor.py
+++ src/thu_network_autoauth/monitor.py
@@ -5,7 +5,7 @@ from .log import logger
 def check_ip_available(ip: str) -> bool:
     try:
         delay = ping3.ping(ip, timeout=2)
-        return delay is not None
+        return delay is not None and delay is not False
     except Exception as e:
         logger.error(f"Error checking IP {ip}: {e}")
         return False
